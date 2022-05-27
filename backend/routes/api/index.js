8a26052473464ca4a4b8e2ecf6ffe796d5cc2109
Huydu Tran <duutran87@icloud.com>
2022-05-27 13:56:27 -0700

    added user authen middlewares

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
+
 
 module.exports = router;
