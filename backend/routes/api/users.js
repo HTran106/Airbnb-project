4a7d581b5efd35a88084c5f25ca422e9ca742338
Huydu Tran <duutran87@icloud.com>
2022-05-27 14:57:18 -0700

    finished setting up backend, need to work on routes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { validateSignup } = require('../../utils/validation')
 const { User } = require('../../db/models');
 const router = express.Router();
 
 //Sign up route
-router.post('/', async (req, res) => {
+router.post('/',  validateSignup, async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
     const user = await User.signup({ firstName, lastName, email, password });
 
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -34,4 +34,24 @@ const validateLogin = [
   handleValidationErrors
 ];
 
-module.exports = { handleValidationErrors, validateLogin }
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 2 })
+    .withMessage('Please provide a valid first name with at least 2 characters'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 2})
+    .withMessage('Please provide a valid last name with at least 2 characters'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+module.exports = { handleValidationErrors, validateLogin, validateSignup }
