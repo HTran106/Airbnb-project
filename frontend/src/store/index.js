cf8e4c44c615d3ebcfdfb095ad893933ccf66b79
Huydu Tran <duutran87@icloud.com>
2022-09-16 11:47:47 -0700

    starting components

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -11,7 +11,7 @@ const rootReducer = combineReducers({
   spots: spotsReducer,
   reviews: reviewsReducer,
   bookings: bookingsReducer,
-  images, imagesReducer
+  images: imagesReducer
 });
 
 let enhancer;
