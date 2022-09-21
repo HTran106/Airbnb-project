684a81aa4b162feba584256af14db8e8e4707cc4
Huydu Tran <duutran87@icloud.com>
2022-09-20 22:02:18 -0700

    luxe difference done

--- frontend/src/components/HomePageComponent/LuxeDifferenceComponent/index.js
+++ frontend/src/components/HomePageComponent/LuxeDifferenceComponent/index.js
@@ -17,10 +17,10 @@ const LuxeDifferenceComponent = () => {
                     <div style={{ fontWeight: '600' }} className='amenities-box-words'>Luxury amenities</div>
                     <div className='amenities-box-words'>Fully equipped to meet your needs, with ample space and privacy</div>
                 </div>
-                <div className='amenities-box'>
+                <div className='amenities-box-2'>
                     <div className="fa-solid fa-clipboard-list fa-xl amenities-box-content"></div>
                     <div style={{ fontWeight: '600' }} className='amenities-box-words'>Custom itineraries</div>
-                    <div style={{ width: '100%' }} className='amenities-box-words'>Your trip designer can plan every last detail and make sure everything is just right</div>
+                    <div className='amenities-box-words'>Your trip designer can plan every last detail and make sure everything is just right</div>
                 </div>
             </div>
             <div style={{ paddingTop: '2em' }} className='amenities'>
@@ -34,7 +34,7 @@ const LuxeDifferenceComponent = () => {
                     <div style={{ fontWeight: '600' }} className='amenities-box-words'>Effortless arrivals</div>
                     <div className='amenities-box-words'>Statement-making homes with exceptionally styled interiors</div>
                 </div>
-                <div className='amenities-box'>
+                <div className='amenities-box-2'>
                     <div className="fa-solid fa-kitchen-set fa-xl amenities-box-content"></div>
                     <div style={{ fontWeight: '600' }} className='amenities-box-words'>Tailored services</div>
                     <div className='amenities-box-words'>Statement-making homes with exceptionally styled interiors</div>
