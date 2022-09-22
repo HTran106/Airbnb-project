e60db4244d072742aa9960d48a97fa833694f0f3
Huydu Tran <duutran87@icloud.com>
2022-09-21 18:30:26 -0700

    fixed minor css

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { SignUpModal } from '../../context/Modal';
 import SignupForm from './SignupForm';
-// import { useSpring, animated } from 'react-spring'
+import { useSpring, animated } from 'react-spring'
 
 function SignupFormModal({ setOpenMenu }) {
     const [showModal, setShowModal] = useState(false);
 
-    // const animation = useSpring({
-    //     config: {
-    //         duration: 250
-    //     },
-    //     opacity: showModal ? 1 : 0,
-    //     transform: showModal ? `translateY(0%)` : `translateY(-100%)`
-    // })
+    const animation = useSpring({
+        config: {
+            duration: 250
+        },
+        opacity: showModal ? 1 : 0,
+        transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+    })
 
     return (
         <>
@@ -20,16 +20,16 @@ function SignupFormModal({ setOpenMenu }) {
                 <button className='signout-button' onClick={() => setShowModal(true)}>Sign Up</button>
             </div>
             {showModal && (
-                <div>
-                    {/* <animated.div style={animation}> */}
+                // <div>
+                <animated.div style={animation}>
                     <SignUpModal onClose={() => {
                         setShowModal(false)
                         setOpenMenu(false)
                     }}>
                         <SignupForm />
                     </SignUpModal>
-                    {/* </animated.div> */}
-                </div>
+                </animated.div>
+                // </div>
             )
             }
         </>
