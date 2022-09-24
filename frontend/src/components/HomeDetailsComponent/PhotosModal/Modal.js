2a29d862716e2fe40bb22e5a89f8f78e1a0c6c8a
Huydu Tran <duutran87@icloud.com>
2022-09-23 20:08:49 -0700

    photos modal needs bottom portion

--- frontend/src/components/HomeDetailsComponent/PhotosModal/Modal.js
+++ frontend/src/components/HomeDetailsComponent/PhotosModal/Modal.js
@@ -21,7 +21,16 @@ const PicModal = ({ showModal, setShowModal, images }) => {
         }
     }
 
-    console.log(images)
+    const handleRight = e => {
+        e.preventDefault()
+        setI(prev => prev + 1)
+    }
+
+    const handleLeft = e => {
+        e.preventDefault()
+        setI(prev => prev - 1)
+    }
+
 
     return (
         <>
@@ -31,12 +40,13 @@ const PicModal = ({ showModal, setShowModal, images }) => {
                         <div className='modal-wrapper'>
                             {/* <img className='photos-modal-img' src={'http://2.bp.blogspot.com/-6puRWRY2UGY/UeeGtTaUACI/AAAAAAAAAto/hmFQyMpD0d8/s1600/luxury+homes4.jpg'} alt='preview' /> */}
                             <div className='photos-modal-content'>
-                                {i !== 1 ? (<div className='arrow-container'><button className='fa-solid fa-angle-left arrow-button'></button></div>) : (<div></div>)}
+                                {i !== 0 ? (<div className='arrow-container'><button onClick={handleLeft} className='fa-solid fa-angle-left arrow-button'></button></div>) : (<div></div>)}
                                 <div className='current-image'>
                                     <img className='mid-image' src={images[i].url} alt='preview' />
                                 </div>
-                                {i !== images.length - 1 ? (<div className='arrow-container'><button className='fa-solid fa-angle-right arrow-button'></button></div>) : (<div></div>)}
+                                {i !== images.length - 1 ? (<div className='arrow-container'><button onClick={handleRight} className='fa-solid fa-angle-right arrow-button'></button></div>) : (<div></div>)}
                             </div>
+                            <div style={{ color: 'white' }}>HEY</div>
                             <button className='fa solid fa-x close-button' aria-label='Close modal' onClick={() => setShowModal(prev => !prev)}></button>
                         </div>
                     </animated.div>
