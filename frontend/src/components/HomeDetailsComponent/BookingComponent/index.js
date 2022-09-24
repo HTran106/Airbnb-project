0c36a74f7cbb8dcbd91935c020e45b132fce5b10
Huydu Tran <duutran87@icloud.com>
2022-09-23 22:31:13 -0700

    working on calendar

--- frontend/src/components/HomeDetailsComponent/BookingComponent/index.js
+++ frontend/src/components/HomeDetailsComponent/BookingComponent/index.js
@@ -1,17 +1,37 @@
 import './BookingComponent.css';
+import Calendar from 'react-calendar';
+import './Calendar.css';
+import { useState } from 'react';
 
 const BookingComponent = () => {
+    const [checkInValue, setCheckInValue] = useState(new Date());
+    const [checkOutValue, setCheckOutValue] = useState(new Date());
+
+    const tileDisabled = ({ activeStartDate, date, view }) => {
+        return date < new Date()
+    }
+
+    const tileDisabled2 = ({ activeStartDate, date, view }) => {
+        return date < checkInValue && date < new Date()
+    }
+
     return (
         <div className="booking-component-container">
-            <div className='start-calendar'>
-                <span className="select-checkin-words">Select check-in date</span>
+            <div className='calendar'>
+                <div className='checkin-word-container'>
+                    <span className="select-checkin-words">Select check-in date</span>
+                </div>
+                <Calendar tileDisabled={tileDisabled} onChange={setCheckInValue} value={checkInValue} />
                 <h1>hey</h1>
                 <h1>hey</h1>
                 <h1>hey</h1>
                 <h1>hey</h1>
             </div>
-            <div className="end-calendar">
-
+            <div className='calendar'>
+                <div className='checkin-word-container'>
+                    <span className="select-checkin-words">Select check-out date</span>
+                </div>
+                <Calendar tileDisabled={tileDisabled2} onChange={setCheckOutValue} value={checkOutValue} />
             </div>
             <div className="booking-card">
 
