63a4c5f09c67905d8a4afcbc5d9b10082cda0381
Huydu Tran <duutran87@icloud.com>
2022-09-25 13:51:32 -0700

    testing deployment

--- frontend/src/components/HomeDetailsComponent/GoogleMapsComponent/index.js
+++ frontend/src/components/HomeDetailsComponent/GoogleMapsComponent/index.js
@@ -3,7 +3,7 @@ import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { useMemo } from 'react';
 
 const GoogleMapComponent = () => {
-    // console.log(process.env.REACT_APP_HEY)
+    console.log(process.env.REACT_APP_NEXT_PUBLIC_GOOGLE_MAPS_API_KEY)
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
     })
