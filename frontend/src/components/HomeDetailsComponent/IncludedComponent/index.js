8781513488f5d7150640d01e2e07b1b197e5abea
Huydu Tran <duutran87@icloud.com>
2022-09-25 17:11:13 -0700

    done with amenities

--- frontend/src/components/HomeDetailsComponent/IncludedComponent/index.js
+++ frontend/src/components/HomeDetailsComponent/IncludedComponent/index.js
@@ -1,5 +1,16 @@
 import './IncludedComponent.css';
 
+const images = [
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/bartender.jpeg Bartender",
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/butler.jpeg Butler",
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/chef.jpeg Chef",
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/childcare.jpeg Childcare",
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/driver.jpeg Driver",
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/housekeeping.jpeg Housekeeping",
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/security.jpeg Security",
+    "https://airbnb-files.s3.us-west-1.amazonaws.com/spa.jpeg Spa",
+]
+
 const IncludedComponent = () => {
     return (
         <>
@@ -7,14 +18,18 @@ const IncludedComponent = () => {
                 <div className='included-with-home-container'>
                     <span className='included-with-home-word'>Included with this home</span>
                 </div>
-                <div>
-                    <span>Helpful essentials you can expect when booking this home</span>
+                <div className='included-with-home-container'>
+                    <span className='included-essentials'>Helpful essentials you can expect when booking this home</span>
                 </div>
-                <div>
-
+                <div className='images'>
+                    {images?.map((imgSrc, i) => (
+                        <div key={i} className='image-container'>
+                            <img className='image' src={imgSrc.split(" ")[0]} alt='included' />
+                            <span>{imgSrc.split(" ")[1]}</span>
+                        </div>
+                    ))}
                 </div>
             </div>
-            <h1>HEY</h1><h1>HEY</h1><h1>HEY</h1><h1>HEY</h1><h1>HEY</h1>
         </>
     )
 }
