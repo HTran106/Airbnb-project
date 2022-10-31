5f33a76b8a22f49a567f8c797556e90428369983
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-10-31 14:46:00 -0700

    done

--- frontend/src/components/Navigation/LoginButton/index.js
+++ frontend/src/components/Navigation/LoginButton/index.js
@@ -8,7 +8,7 @@ import { useHistory } from 'react-router-dom'
 import CreateSpotFormModal from '../CreateSpotModal'
 
 
-const LoginButton = ({setShowPopup, openPopup }) => {
+const LoginButton = ({ openPopup }) => {
     const dispatch = useDispatch()
     const history = useHistory()
     const [openMenu, setOpenMenu] = useState(false)
